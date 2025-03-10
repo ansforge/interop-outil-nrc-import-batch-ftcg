83df40bd8391209abdd03a5333956ea1bdd9aeaa
legallm <mael.le_gall@tutanota.com>
2025-03-10 16:51:49 +0100

    Optimisation des retours visuels

--- import_batch_ftcg/main.py
+++ import_batch_ftcg/main.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
 
     # Lecture et pré-processus de la Common French
     cf = io.read_common_french(args.cf_path, args.cf_date, args.fr_path, args.fr_date)
-    print(f"Common French extraite : {len(cf)} lignes.")
+    print(f"\nCommon French extraite : {len(cf)} lignes.")
     # Lecture et pré-processus de l'édition nationale
     fr = io.get_fr_edition(args.fr_path, args.fr_date, args.unpub_fr_path)
     print(f"Edition nationale extraite : {len(fr)} lignes.")
@@ -27,11 +27,13 @@ if __name__ == "__main__":
     # Conserve seulement les traduction de la Common French pour des concepts dans
     # l'édition nationale n'ayant pas de description FR (active ou inactive)
     cf = cf.loc[~cf.loc[:, "conceptId"].isin(fr.loc[:, "conceptId"])]
+    print(f"Réduction de la Common French à importer : {len(cf)} lignes d'intérêt.")
 
     # Vérification des règles pour relecture
-    print("Vérification du respect des règles éditoriales en cours...")
+    print("\nVérification du respect des règles éditoriales en cours...")
     cf = control.run_quality_control(cf)
     print("Vérification terminée")
 
     # Ecriture du fichier d'import
     io.write_batch_file(cf, args.output)
+    print(f"\nFichier final sauvegardé : {args.output}")
